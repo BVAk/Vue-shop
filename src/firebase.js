869d1e0e5a91f4e74ab2ec4be40c326df2c01b94
BVAk <bekkviktoria55@gmail.com>
2019-08-19 21:51:13 +0300

    add firebase and login

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import firebase from 'firebase';
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyAxHDbSdG4nlXHz06MfljsRVOIl8HxJHtY",
+    authDomain: "vue-shop-75d97.firebaseapp.com",
+    databaseURL: "https://vue-shop-75d97.firebaseio.com",
+    projectId: "vue-shop-75d97",
+    storageBucket: "",
+    messagingSenderId: "61635332534",
+    appId: "1:61635332534:web:37e35ed513d373ab"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
